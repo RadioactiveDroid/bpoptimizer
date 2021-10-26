1c24626793f8719fe1f2bb4d9204ebd7b4a9e462
RadioactiveDroid (via Travis CI) <radioactivedroid@gmail.com>
2021-10-26 09:13:56 +0000

    Deploy RadioactiveDroid/bpoptimizer to github.com/RadioactiveDroid/bpoptimizer.git:gh-pages

--- bpoptimizer/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Top-level package for BlockParty Floor Optimizer."""
-
-__author__ = """RadioactiveDroid"""
-__email__ = "radioactivedroid@gmail.com"
-__version__ = "1.3.0"
-
-
-from .floor import Floor
